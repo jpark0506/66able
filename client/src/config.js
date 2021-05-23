8ab2bb03245fbb07abf93fa4564f827472d4b641
Ldj-git <68588092+Ldj-git@users.noreply.github.com>
2021-05-23 13:54:18 +0900

    habit crud
    habit 관련 작업 완료

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,5 @@
-const REST_API_KEY="351d2b8fedd2b491486182039c85736e"
-const REDIRECT_URI="http://localhost:5000/kakao/oauth/callback"
-module.exports={REST_API_KEY, REDIRECT_URI}
+// const REST_API_KEY="351d2b8fedd2b491486182039c85736e"
+const REST_API_KEY = "ddf9cdac8fa0b1ff5990e353248fc08d";
+
+const REDIRECT_URI = "http://localhost:5000/kakao/oauth/callback";
+module.exports = { REST_API_KEY, REDIRECT_URI };
---
+++ server/models/habit.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+const {
+  Types: { ObjectId },
+} = Schema;
+const habitSchema = new Schema({
+  habitname: { type: String, required: true },
+  creator: { type: ObjectId, ref: "User" },
+  users: [{ type: ObjectId, ref: "User" }],
+  objective: { type: String },
+  description: { type: String },
+  startdate: { type: Date },
+  reaction: [
+    {
+      wronghabit: { type: String },
+      habit: { type: String },
+      signal: { type: String },
+      habittime: [{ type: String }],
+      habitimage: { type: String },
+      before: [{ type: Number }],
+      success: [{ type: Date }],
+      habittype: { type: Number },
+    },
+  ],
+  public: { type: Number },
+  tag: [{ type: String }],
+});
+const Habit = mongoose.model("Habit", habitSchema);
+module.exports = { Habit };
